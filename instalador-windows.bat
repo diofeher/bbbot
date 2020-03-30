3dc6c69261e42a18c1f621c3e286c265a6b0fdcd
Diógenes Augusto Fernandes Hermínio <diofeher@gmail.com>
2020-03-30 03:03:24 -0300

    missing files

---
+++ instalador-windows.bat
@@ -0,0 +1,2 @@
+npm install
+pip3 install -r requirements-windows.txt
---
+++ rodar-windows.bat
@@ -0,0 +1,5 @@
+SET /P GLOBO_USERNAME= Digite o e-mail da Globo:
+SET /P GLOBO_PASSWORD= Digite a senha da Globo:
+SET /P PARTICIPANTE= Digite o participante que vc quer votar: (1 pra Prior, 2 pra Manu e 3 pra Mari)
+
+npm start
